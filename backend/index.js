21a34688649662ff3e9fbb48d8567fa3ed92eb3f
Percy_jacker <68894634+PercyJacker@users.noreply.github.com>
2024-11-30 22:32:49 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -24,14 +24,8 @@ require('dotenv').config();
 ffmpeg.setFfmpegPath(ffmpegPath);
 
 
-app.use(cors({
+app.use(cors()); // Allows all origins
 
-  origin:[ "https://intern-azure.vercel.app"], // Vercel domain of your frontend
-  methods: ["GET", "POST"],
-  allowedHeaders: ["Content-Type"],
-credentials: true
-
-}));
 app.use(bodyParser.json({limit:"50mb"}))
 app.use(bodyParser.urlencoded({extended:true,limit:"50mb"}))
 app.use(express.json())
