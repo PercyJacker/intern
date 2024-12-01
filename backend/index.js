7525d51d3f05910d0f320374e7e54377f1d30de1
Percy_jacker <68894634+PercyJacker@users.noreply.github.com>
2024-12-01 08:27:04 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -25,12 +25,8 @@ ffmpeg.setFfmpegPath(ffmpegPath);
 
 
 // Use the CORS middleware
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*"); // Allow all origins
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-});
+app.use(cors()); // Automatically sets CORS headers
+
 
 
 
