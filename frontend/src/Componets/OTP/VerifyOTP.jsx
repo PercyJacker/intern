baaf882306d5fc7c59bb2df43e4163096735a5a8
Percy_jacker <68894634+PercyJacker@users.noreply.github.com>
2024-12-03 12:05:22 +0530

    Update VerifyOTP.jsx

--- frontend/src/Componets/OTP/VerifyOTP.jsx
+++ frontend/src/Componets/OTP/VerifyOTP.jsx
@@ -9,7 +9,7 @@ const VerifyOTP = () => {
   const handleRequestOTP = async (e) => {
     e.preventDefault();
     // const backendURL = "https://intern-8hmo.vercel.app";
-  axios.post('https://intern-8hmo.vercel.app/api/send-otp', { email })
+  axios.post(`https://intern-okm8.vercel.app/api/send-otp`, { email })
   .then(response => {
       alert(response.data);
     setShowVerifyForm(true);
